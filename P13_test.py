032025fdb79a92caf96982b1d030c39096108c07
Michal23 <michal.wegrzyn2137@gmail.com>
2023-01-11 23:09:10 +0100

    Minor cleanup after self-review

--- P13_test.py
+++ P13_test.py
@@ -6,9 +6,6 @@ E0, E1, E2, E3, E4, E5, I1, I2, I3, I4, I5 = next_nodes(11)
 
 if __name__ == "__main__":
     g1 = Graph()
-    # g1.add_nodes_from([(E0, attr("E", 0, 10)),
-    #     (I1, attr("I")), (I2, attr("I")), (I3, attr("I")), (I4, attr("I")), (E1, attr("E", -1, -1)), (E2, attr("E", 0, 0)), (E3, attr("E", 1, 1)),
-    #     (I5, attr("I")), (I6, attr("I")), (E4, attr("E", -1, -1)), (E5, attr("E", 1, 1))])
     g1.add_nodes_from([(E0, attr("E", 0, 6)),
         (I1, attr("I", -1, 4)), (I2, attr("I", -2, 1)), (I3, attr("I", -2, -1)), (I4, attr("I", 1, 4)), (E1, attr("E", 0, 1)), (E2, attr("E", 0, 0)), (E3, attr("E", 0, -1)),
         (I5, attr("I", 2, 0)), (E4, attr("E", 0, 1)), (E5, attr("E", 0, -1))])
--- productions/P1.py
+++ productions/P1.py
@@ -22,4 +22,4 @@ def production_modification(graph: Graph, mapping: dict):
     graph.add_nodes_from([(E1, attr("E", 0, 0, level)), (E2, attr("E", 0, 1, level)), (E3, attr("E", 1, 1, level)), (E4, attr("E", 1, 0, level)), (I, attr("I", 0.5, 0.5, level))])
     graph.add_edges_from([(E1, E2), (E2, E3), (E3, E4), (E4, E1), (E1, I), (E2, I), (E3, I), (E4, I), (mapping[EL], I)])
 
-P1 = Production(production_left_side(), production_modification, None)
+P1 = Production(production_left_side(), production_modification)
